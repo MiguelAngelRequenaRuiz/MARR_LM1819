539a3bcc526067c509a6524efcea8eb3c80cf91b
Miguel Angel <miguelangelrequenaruiz@gmail.com>
2019-03-17 13:28:29 +0100

    repaso

--- PHP/Apuntes/d. Formularios/Formulario1/resultado.php
+++ PHP/Apuntes/d. Formularios/Formulario1/resultado.php
@@ -1,8 +1,17 @@
 <?php
 
-strip_tags($_REQUEST["nombre"]  # C
+strip_tags($_REQUEST["nombre"]  # strip_tags() sirve para que no funcione el html
+                                # por ejemplo que si pones <b></b> no aparezca en negrita
 
+trim()      #quita los espacio que haya por delante y por detras
+
+htmlspecialchars(cadena, ENT_QUOTES, "utf-8")     #Convierte las comillas dobles en simples 
+
+
+//Para trabajar mejor hacer una variable
 $nombre = trim(htmlspecialchars(strip_tags($_REQUEST["nombre"]), ENT_QUOTES, "UTF-8"));
+//Con esta cadena quitamos los espacios por delante y por detras, los caracteres especiales y las etiquetas html
+
 if ($nombre != "") {
     print "<p>Su nombre es $nombre</p>";
 }
---
+++ PHP/Apuntes/d. Formularios/Formulario2/procesarEdad.php
@@ -0,0 +1,16 @@
+<?php
+$edad = $_REQUEST["edad"];
+$email = $_REQUEST["email"];
+if (is_numeric($edad)) {            #comprueba que es un numero
+    print "Su edad es $edad";
+} else {
+    print "Error al introducir la edad";
+}
+
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {    #filter_var(cadena, tipo_validacion)    
+    print "Email correcto";                         #Tipos de validacion: FILTER_VALIDATE_URL, FILTER_VALIDATE_INT (enteros)
+} else {
+    print "Email incorrecto";
+}
+?>
